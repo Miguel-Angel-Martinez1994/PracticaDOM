6b53fd10e815a4826ca5acd4ccf350b22d03722e
Miguel Martinez <mamg1603@gmail.com>
2024-10-01 16:52:16 +0200

    ADD: pseudocodigo y galeria FINISH: js

--- js/main.js
+++ js/main.js
@@ -1,8 +1,9 @@
-//asignacion de los elementos del HTML
+//asignacion de los elementos del HTML necesarios
 const imagenAlt=document.querySelector("#altIMG");
+const galeria=document.querySelector("#galeria");
 
 //array de las imagenes
-const array=[
+const galeriaFotos=[
     {
         id:1,
         title:'titulo imagen1',
@@ -47,20 +48,41 @@ const array=[
     }
 ]
 
+//preparamos el fragment para guardar las fotos creadas
+const fragment=document.createDocumentFragment();
+
 const imagenRandom=()=>{
     //generamos numero aleatorio y variables auxiliares
-    let aleatorio = Math.floor(Math.random() * array.length);
+    let aleatorio = Math.floor(Math.random() * galeriaFotos.length);
 
     //asignamos a imagenALT el src y alt, en base al numero random
     imagenAlt.classList.add("src");
     imagenAlt.classList.add("alt");
 
-    imagenAlt.src=array[aleatorio].src;
-    imagenAlt.alt=array[aleatorio].alt;
+    imagenAlt.src=galeriaFotos[aleatorio].src;
+    imagenAlt.alt=galeriaFotos[aleatorio].alt;
 }
 
 const galeriaImagenes=()=>{
+    //recorremos el array de imagenes y crearemos una <img> para cada uno
+    galeriaFotos.forEach((posicion)=>{
+        //creamos una etiqueta imagen para cada foto de la galeria
+        const imagenGaleria=document.createElement("IMG");
 
+        //creamos las clases que vamos a usar y asignar
+        imagenGaleria.classList.add("src");
+        imagenGaleria.classList.add("alt");
+        imagenGaleria.classList.add("tittle");
+
+        imagenGaleria.src=posicion.src;
+        imagenGaleria.alt=posicion.alt;
+        imagenGaleria.title=posicion.title;
+
+        fragment.append(imagenGaleria);
+    })
+
+    //mostramos todas las imagenes al final del div galeria
+    galeria.append(fragment);
 }
 
 //funcion anonima auto-ejecutable para arrancar las funciones
